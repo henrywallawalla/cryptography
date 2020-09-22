a480a1be570dfbc4c6b51826e60b92a1592c5784
henrywallawalla <henry.wallace@menloschool.org>
2020-09-22 13:50:09 -0700

    henry's

---
+++ crypto.py
@@ -0,0 +1,62 @@
+def alphabet_wrap(startchar, shiftvalue):
+    if(ord(startchar) + shiftvalue > ord("Z"))
+        
+
+
+# Caesar Cipher
+# Arguments: string, integer
+# Returns: string
+def encrypt_caesar(plaintext, offset):
+    encrypted = ""    
+    if(plaintext.length == 0):
+        return encrypted
+    for char in plaintext:
+        if(ord("A") <= ord(char) <= ord("Z")):
+            encrypted += alphabet_wrap(char, offset)
+    return encrypted
+
+            
+
+# Arguments: string, integer
+# Returns: string
+def decrypt_caesar(ciphertext, offset):
+    pass
+
+# Vigenere Cipher
+# Arguments: string, string
+# Returns: string
+def encrypt_vigenere(plaintext, keyword):
+    pass
+
+# Arguments: string, string
+# Returns: string
+def decrypt_vigenere(ciphertext, keyword):
+    pass
+
+# Merkle-Hellman Knapsack Cryptosystem
+# Arguments: integer
+# Returns: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
+def generate_private_key(n=8):
+    pass
+
+# Arguments: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
+# Returns: tuple B - a length-n tuple of integers
+def create_public_key(private_key):
+    pass
+
+# Arguments: string, tuple (W, Q, R)
+# Returns: list of integers
+def encrypt_mhkc(plaintext, public_key):
+    pass
+
+# Arguments: list of integers, tuple B - a length-n tuple of integers
+# Returns: bytearray or str of plaintext
+def decrypt_mhkc(ciphertext, private_key):
+    pass
+
+def main():
+    # Testing code here
+    pass
+
+if __name__ == "__main__":
+    main()
